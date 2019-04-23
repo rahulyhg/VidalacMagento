1889a0668f6df91e1235f77b1390b1c4601c3f45
MrCrawled <fertepsi@gmail.com>
2019-04-23 11:31:12 -0300

    Actualizaciones

--- app/design/frontend/Coso.1/coso/registration.php
+++ app/design/frontend/Coso.1/coso/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Coso/coso',
+    'frontend/Coso.1/coso',
     __DIR__
 );
--- app/design/frontend/Probando.1/probando/registration.php
+++ app/design/frontend/Probando.1/probando/registration.php
@@ -5,6 +5,6 @@
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Probando/probando',
+    'frontend/Probando.1/probando',
     __DIR__
 );
